d0ed626b5ee351bb6e58932bf6cfa7e92ac728d3
aakanshanarain <aakanshanarain@gmail.com>
2022-03-23 10:50:48 +0800

    Add preliminary login page prototype with cors authentication

---
+++ src/components/App/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import './App.css';
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
+import Subject from '../Subject/Subject';
+import useToken from './useToken';
+
+
+function App() {
+    const { token, setToken } = useToken();
+
+    if(!token) {
+        return <Login setToken={setToken} />
+    }
+
+    return (
+        <div className="wrapper">
+            <Dashboard />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/Dashboard" element={<Dashboard />} />
+                    <Route path="/Subject" element={<Subject /> } />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Subject/Subject.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Subject() {
+    return(
+        <h2>Science</h2>
+    );
+}
