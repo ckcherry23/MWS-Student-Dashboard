3db0f32d954aa2ee47526ca1d99a40df84fb12c9
Charisma Kausar <charisma.kausar@gmail.com>
2022-03-26 14:40:50 +0800

    Add navigation

---
+++ src/components/Subject/Chapter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Subject() {
+  return (
+    <div>
+      <h2>Cells</h2>
+    </div>
+  );
+}
--- src/components/Subject/Subject.js
+++ src/components/Subject/Subject.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
+import Chapter from "./Chapter.js"
+import chapterData from "./chapters.json";
+import { useNavigate } from "react-router-dom";
 
 export default function Subject() {
-    return(
-        <h2>Science</h2>
+    const getChapters = () => {
+        fetch('data/chapters.json');
+        console.log()
+    }
+    const navigate = useNavigate();
+    return (
+        <div>
+            <h2>Science</h2>
+            <Chapter />
+            <button type="button" onClick={() => navigate(-1)}>
+                Back
+            </button>
+        </div>
     );
 }
