6d8b255d38f5dd0fb8457c9b120fe507947a504b
Aakansha Narain <30730195+aakanshanarain@users.noreply.github.com>
2022-03-26 15:24:00 +0800

    Revert "Revert "Add navigation and local data reads""

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+// import Subject from "../Subject/Subject.js";
 
 export default function Dashboard() {
-    return(
-        <h2>Welcome!</h2>
+    const navigate = useNavigate();
+    return (
+        <div>
+            <h2>Welcome!</h2>
+            <button type="button" onClick={() => navigate("/subject")}>
+                Science
+            </button>
+        </div>
     );
 }
