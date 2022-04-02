ba279d35c2821f39f5bf21546584958897ed943e
Charisma Kausar <charisma.kausar@gmail.com>
2022-04-03 06:10:45 +0800

    Fix css

--- src/components/Subject/Chapter.js
+++ src/components/Subject/Chapter.js
@@ -3,14 +3,12 @@ import './Chapter.css';
 
 export default function Chapter(props) {
   return (
-    <div className="outer">
     <div className="chapter-wrapper">
       <div className="chapter">
         <div className="section">{props.section.toUpperCase()}</div>
         <div className="title">{props.title}</div>
       </div>
       <img className="chapter-image" src={props.image} alt="Chapter image" />
-    </div>
     </div>
   );
 }
