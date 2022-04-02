4157b797241c40492a6b22d00fd59182180adddb
Charisma Kausar <charisma.kausar@gmail.com>
2022-04-03 01:36:26 +0800

    Show subjects in grid

--- src/components/Subject/Chapter.js
+++ src/components/Subject/Chapter.js
@@ -4,9 +4,11 @@ import './Chapter.css';
 export default function Chapter(props) {
   return (
     <div className="chapter-wrapper">
+      <div className="chapter">
+        <div className="section">{props.section.toUpperCase()}</div>
+        <div className="title">{props.index}. {props.title}</div>
+      </div>
       <img className="chapter-image" src={props.image} alt="Chapter image" />
-      <div className="section">{props.section.toUpperCase()}</div>
-      <div className="title">{props.index}. {props.title}</div>
     </div>
   );
 }
--- src/components/Subject/Subject.js
+++ src/components/Subject/Subject.js
@@ -2,18 +2,24 @@ import React from "react";
 import Chapter from "./Chapter.js";
 import chapterData from "./chapters.json";
 import { useNavigate } from "react-router-dom";
+import './Subject.css';
 
 export default function Subject() {
-    const chapters = JSON.parse(JSON.stringify(chapterData));
+    const chapters = JSON.parse(JSON.stringify(chapterData)).sort((a,b)=> a.index-b.index);
 
     const navigate = useNavigate();
     return (
         <div>
             <h2>Science</h2>
-            {chapters.map((chapter) => <Chapter title={chapter.title} section={chapter.section} index={chapter.index} image={chapter.image}/>)}
-            <button type="button" onClick={() => navigate(-1)}>
+            <button className="back" type="button" onClick={() => navigate(-1)}>
                 Back
             </button>
+            <div className="grid">
+            {chapters.map((chapter) => <div className="grid-item"><Chapter title={chapter.title}
+                section={chapter.section}
+                index={chapter.index}
+                image={chapter.image} /></div>)}
+            </div>
         </div>
     );
 }
